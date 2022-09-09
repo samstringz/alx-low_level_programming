0c3f41009d85c04e71db439a5d667d5fa8f81070
Samstringz <83889735+samstringz@users.noreply.github.com>
2022-09-09 23:17:57 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
-* main - Lanzador
-*
-* Return: Always 0
-*
-*/
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
