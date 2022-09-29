77e3665110c136665db074d9ae283a4c1045f502
samstringz <83889735+samstringz@users.noreply.github.com>
2022-09-29 10:38:29 +0100

    recursion task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion.c - funtion
+ * @s: input
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+	if (*s ** '/0')
+		_putchar('n');
+	else 
+	{
+		_putchar('n');
+		_puts_recursion(s + 1);
+	}
